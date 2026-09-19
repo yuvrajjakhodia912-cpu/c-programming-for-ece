1dcbddab4ddd2175e58c58d611e2891693f9a64a
Yuvraj Jakhodia <yuvrajjakhodia912@gmail.com>
2026-09-19 15:21:10 +0530

    Create 20-multiplication-table.c

---
+++ 01-fundamentals/20-multiplication-table.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int number, i;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("\nMultiplication table of %d:\n", number);
+    for (i = 1; i <= 10; i++){
+        printf("%d x %d = %d\n", number, i, number * i);
+    }
+    return 0;
+}
