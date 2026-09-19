0845b5be245e4516637c89e43cfed86f6a33e6c0
Yuvraj Jakhodia <yuvrajjakhodia912@gmail.com>
2026-09-19 14:07:48 +0530

    Create input-output.c

---
+++ 01-fundamentals/input-output.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int age;
+  int marks;
+  printf("Enter your age:");
+  scanf("%d",&age);
+  printf("Enter your marks:");
+  scamf("%d",&marks);
+  printf("\nAge= %d\n",age);
+  printf("\nMarks =%d\n",marks);
+  return 0;
+}
