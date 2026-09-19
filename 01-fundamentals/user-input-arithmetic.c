345d567b7e8bede69825a97096e2e383a26b956b
Yuvraj Jakhodia <yuvrajjakhodia912@gmail.com>
2026-09-19 14:26:37 +0530

    Create user-input-arithmetic.c

---
+++ 01-fundamentals/user-input-arithmetic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+  int a,b:
+  printf("Enter first number:");
+  scanf("%d",&a);
+  printf("Enter second number:");
+  scanf("%d",&b);
+  printf("\nAddition = %d\n", a + b); 
+  printf("Subtraction = %d\n", a - b); 
+  printf("Multiplication = %d\n", a * b); 
+  printf("Division = %d\n", a / b); 
+  printf("Remainder = %d\n", a % b);
+  return 0;
+}
