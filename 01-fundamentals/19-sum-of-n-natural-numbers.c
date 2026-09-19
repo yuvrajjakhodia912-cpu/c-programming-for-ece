1e1d1cf1e430bacbb8aecc3cb3dceeabe04a892c
Yuvraj Jakhodia <yuvrajjakhodia912@gmail.com>
2026-09-19 15:19:42 +0530

    Create 19-sum-of-n-natural-numbers.c

---
+++ 01-fundamentals/19-sum-of-n-natural-numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++){
+        sum = sum + i;
+    }
+    printf("Sum = %d\n", sum);
+    return 0;
+}
