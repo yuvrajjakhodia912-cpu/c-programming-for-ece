864129729bf38095e099e322e86fa666c65dc71e
Yuvraj Jakhodia <yuvrajjakhodia912@gmail.com>
2026-09-19 14:53:45 +0530

    Create 12-grade-calculator.c

---
+++ 01-fundamentals/12-grade-calculator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    float marks;
+    printf("Enter your marks: ");
+    scanf("%f", &marks);
+    if (marks >= 90){
+        printf("Grade: A\n");
+    }
+    else if (marks >= 80){
+        printf("Grade: B\n");
+    }
+    else if (marks >= 70){
+        printf("Grade: C\n");
+    }
+    else if (marks >= 60){
+        printf("Grade: D\n");
+    }
+    else{
+        printf("Grade: F\n");
+    }
+    return 0;
+}
