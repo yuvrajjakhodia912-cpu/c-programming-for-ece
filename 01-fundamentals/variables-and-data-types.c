7ff68e4b858fd0d0cc28aabaf367a4f4437c56b7
Yuvraj Jakhodia <yuvrajjakhodia912@gmail.com>
2026-09-19 14:16:46 +0530

    Create variables-and-data-types.c

---
+++ 01-fundamentals/variables-and-data-types.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  int age=19;
+  float height=5.10;
+  char grade="A";
+  printf("\nAge= %d\n",age);
+  printf("\nHeight = %.2f feet\n",height);
+  printf("\nGrade = %c\n",garde);
+  return 0;
+}
