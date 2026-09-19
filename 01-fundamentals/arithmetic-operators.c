2adc67663784a0845825268d30b33f19c5b82c40
Yuvraj Jakhodia <yuvrajjakhodia912@gmail.com>
2026-09-19 14:22:14 +0530

    Create arithmetic-operators.c

---
+++ 01-fundamentals/arithmetic-operators.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  int a =25;
+  int b = 9;
+  printf("Addition = %d\n",a+b);
+  printf("Subtraction = %d\n",a-b);
+  printf("Multiplication = %d\n",a*b);
+  printf("Division = %d\n",a/b);
+  printf("Remainder = %d\n",a%b);
+}
